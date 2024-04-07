3cda4ce0544b2106d0f7199b3dd0031ac72bb366
Javier Evans <j.evans@f5.com>
2024-04-07 13:20:51 -0700

    experimenting with decap cms widget for table. bad user experience but it's an option

--- themes/main/assets/js/main.js
+++ themes/main/assets/js/main.js
@@ -1,78 +1,26 @@
-// Demo carousel object
-CMS.registerEditorComponent({
-    id: "carousel",
-    label: "Carousel",
-    fields: [{
-            name: "carousel_header",
-            label: "Carousel Header",
-            widget: "list",
-            types: [
-                {
-                    label: "Carousel",
-                    name: "carousel_item",
-                    widget: "object",
-                    fields: [
-                        {
-                            label: "Images",
-                            name: "images",
-                            widget: "list",
-                            field: {
-                                label: "Image",
-                                name: "image",
-                                widget: "image"
-                            }
-                        }
-                    ]
-                },
-                {
-                    label: "Spotlight",
-                    name: "spotlight",
-                    widget: "object",
-                    fields: [
-                        {
-                            label: "Header",
-                            name: "header",
-                            widget: "string",
-                            default: "Spotlight"
-                        },
-                        { label: "Text", name: "text", widget: "text", default: 'Hello World' }
-                    ]
-                }
-            ]
-        }
-    ],
-    // Additionally, it's recommended that you use non-greedy capturing groups (e.g.
-  // `(.*?)` vs `(.*)`), especially if matching against newline characters.
-  pattern: /^<table>$\s*?<summary>(.*?)<\/summary>\n\n(.*?)\n^<\/table>$/ms,
-    fromBlock: function(match) {
-        console.lot("the fromBlock functionality", match)
-        return {};
-    },
-    toBlock: function(obj) {
-        console.log("the toBlock functionality", obj)
-        return `GORB`;
-    },
-    toPreview: function(obj) {
-        console.log("the toPreview functionality", obj)
-        return 'DOUBLE GORB';
-    },
-});
-
-// Table WIP.  Produces:
+// A hacky table component using decap's inbuilt widget definition.
+// Data looks like this:
 // {
-//   "tableElement": [
+//   "rows": [
 //     {
 //       "type": "headerRow",
-//       "headerRowContents": [
-//         "Dog",
-//         "bear"
+//       "columns": [
+//         "header col1",
+//         "header col 2"
 //       ]
 //     },
 //     {
 //       "type": "row",
-//       "rowContents": [
-//         "Dogs are \n\n* cool\n* great\n* awesome",
-//         "Bears are:\n\n* big\n* scary\n* snooper"
+//       "columns": [
+//         "row 1 col 1",
+//         "row 1 col 2"
+//       ]
+//     },
+//     {
+//       "type": "row",
+//       "columns": [
+//         "row 2 col 1",
+//         "row 2 col 2"
 //       ]
 //     }
 //   ]
@@ -81,61 +29,153 @@ CMS.registerEditorComponent({
     id: "table",
     label: "Table",
     fields: [{
-            name: "tableElement",
-            label: "Table Element",
+      name: "rows",
+      label: "Rows",
+      widget: "list",
+      types: [
+        {
+          label: "Header Row",
+          name: "headerRow",
+          widget: "object",
+          fields: [{
+            label: "Header Column",
+            name: "columns",
             widget: "list",
-            types: [
-                {
-                    label: "Header Row",
-                    name: "headerRow",
-                    widget: "list",
-                    fields: [
-                        {
-                            label: "Header",
-                            name: "headerRowContents",
-                            widget: "list",
-                            field: {
-                                label: "Header Row Content",
-                                name: "headerRowContent",
-                                widget: "string"
-                            }
-                        }
-                    ]
-                },
-                {
-                    label: "Row",
-                    name: "row",
-                    widget: "list",
-                    fields: [
-                        {
-                            label: "Content",
-                            name: "rowContents",
-                            widget: "list",
-                            field: {
-                                label: "Row Content",
-                                name: "rowContent",
-                                widget: "markdown"
-                            }
-                        }
-                    ]
-                }
-            ]
+            field: {
+                label: "Column Name",
+                name: "columnContent",
+                widget: "string"
+              }
+          }]
+        },
+        {
+          label: "Row",
+          name: "row",
+          widget: "object",
+          fields: [{
+            label: "Column",
+            name: "columns",
+            widget: "list",
+            field: {
+                label: "Content",
+                name: "columnContent",
+                widget: "markdown"
+              }
+          }]
         }
-    ],
-    // Additionally, it's recommended that you use non-greedy capturing groups (e.g.
-  // `(.*?)` vs `(.*)`), especially if matching against newline characters.
-  pattern: /^<table>$\s*?<summary>(.*?)<\/summary>\n\n(.*?)\n^<\/table>$/ms,
-    fromBlock: function(match) {
-        console.lot("the fromBlock functionality", match)
-        return {};
+      ]
+    }],
+  // Something is wrong here.. 
+  pattern: /^<table>(?:\s*<thead>(?<thead>[\s\S]*?)<\/thead>)?(?:\s*<tbody>(?<tbody>[\s\S]*?)<\/tbody>)?\s*<\/table>$/gms,
+  // This code uses the match provided by the `pattern` property
+  // to parse out the data and reassemble the javascript object to match the 
+  // `fields` above.  This populates the widget in the CMS WSYIWYG editor
+  fromBlock: function(match) {
+        console.log("match in fromBlock", match);
+        const thContentsRegex = /<th[^>]*>(.*?)<\/th>/gm;
+        const trContentsRegex = /<tr>(.*?)<\/tr>/g;
+        const tdContentsRegex = /<td>(.*?)<\/td>/g;
+        const { thead, tbody } = match.groups;
+
+        const headerRow = {
+            type: "headerRow",
+            columns: [...thead.matchAll(thContentsRegex)].map((match) => {
+                if (match && match[1]) {
+                    return match[1];
+                }
+            })
+        };
+
+        const rowsMarkup = tbody.match(trContentsRegex);
+
+        if (!rowsMarkup) return {};
+
+        const rows = rowsMarkup.map((html) => {
+            return {
+                type: "row",
+                columns: [...html.matchAll(tdContentsRegex)].map((match) => {
+                if (match && match[1]) {
+                    return match[1];
+                }
+              })
+            };
+        })
+        const result = { rows: [headerRow].concat(rows)};
+        console.log("the fromBlock functionality", result);
+        return result;
     },
+    // This is the code that produces the markdown. In our case we just
+    // Generate html
     toBlock: function(obj) {
-        console.log("the toBlock functionality", obj)
-        return `GORB`;
+        console.log("the toBlock functionality", obj);
+        if (!obj.rows) return "";
+
+        const rowsMarkup = obj.rows.reduce((acc, row) => {
+            if (!row.columns) return acc;
+
+            console.log("A ROW: ", row);
+            if (row.type === "headerRow") {
+                acc.headerRows
+                    .push(`<tr>${row.columns.map((col) => '<th scope="col">' + col + '</th>')}</tr>`);
+            } else {
+                acc.rows
+                    .push(`<tr>${row.columns.map((col) => '<td>' + col + '</td>')}</tr>`);
+            }
+
+            return acc;
+        }, {
+            headerRows: [],
+            rows: []
+        });
+
+        const theaderMarkup = rowsMarkup.headerRows ? `<thead>${rowsMarkup.headerRows.join("")}</thead>` : "";
+        const tbodyMarkup = rowsMarkup.rows ?`<tbody>${rowsMarkup.rows.join("")}</tbody>` : "";
+        
+        
+        if (!theaderMarkup && !tbodyMarkup) return "";
+        const markup = `
+        <table>
+            ${theaderMarkup}
+            ${tbodyMarkup}
+        </table>
+        `;
+        return markup;
     },
+    // This creates the preview that should appear above the widget
     toPreview: function(obj) {
         console.log("the toPreview functionality", obj)
-        return 'DOUBLE GORB';
+                if (!obj.rows) return "";
+
+        const rowsMarkup = obj.rows.reduce((acc, row) => {
+            if (!row.columns) return acc;
+
+            console.log("A ROW: ", row);
+            if (row.type === "headerRow") {
+                acc.headerRows
+                    .push(`<tr>${row.columns.map((col) => '<th scope="col">' + col + '</th>')}</tr>`);
+            } else {
+                acc.rows
+                    .push(`<tr>${row.columns.map((col) => '<td>' + col + '</td>')}</tr>`);
+            }
+
+            return acc;
+        }, {
+            headerRows: [],
+            rows: []
+        });
+
+        const theaderMarkup = rowsMarkup.headerRows ? `<thead>${rowsMarkup.headerRows.join("")}</thead>` : "";
+        const tbodyMarkup = rowsMarkup.rows ?`<tbody>${rowsMarkup.rows.join("")}</tbody>` : "";
+        
+        
+        if (!theaderMarkup && !tbodyMarkup) return "";
+        const markup = `
+        <table>
+            ${theaderMarkup}
+            ${tbodyMarkup}
+        </table>
+        `;
+        return markup;
     },
 });
 
