e382866c014bc883798bc4f91d810a84154d930f
Javier Evans <j.evans@f5.com>
2024-04-06 17:16:34 -0700

    protoype widgets to create tables using Decap's strategy

--- themes/main/assets/js/main.js
+++ themes/main/assets/js/main.js
@@ -1,5 +1,4 @@
-(() => {
-    // https://decapcms.org/docs/custom-widgets/#registereditorcomponent
+// Demo carousel object
 CMS.registerEditorComponent({
     id: "carousel",
     label: "Carousel",
@@ -58,5 +57,88 @@ CMS.registerEditorComponent({
         return 'DOUBLE GORB';
     },
 });
-})();
+
+// Table WIP.  Produces:
+// {
+//   "tableElement": [
+//     {
+//       "type": "headerRow",
+//       "headerRowContents": [
+//         "Dog",
+//         "bear"
+//       ]
+//     },
+//     {
+//       "type": "row",
+//       "rowContents": [
+//         "Dogs are \n\n* cool\n* great\n* awesome",
+//         "Bears are:\n\n* big\n* scary\n* snooper"
+//       ]
+//     }
+//   ]
+// }
+CMS.registerEditorComponent({
+    id: "table",
+    label: "Table",
+    fields: [{
+            name: "tableElement",
+            label: "Table Element",
+            widget: "list",
+            types: [
+                {
+                    label: "Header Row",
+                    name: "headerRow",
+                    widget: "list",
+                    fields: [
+                        {
+                            label: "Header",
+                            name: "headerRowContents",
+                            widget: "list",
+                            field: {
+                                label: "Header Row Content",
+                                name: "headerRowContent",
+                                widget: "string"
+                            }
+                        }
+                    ]
+                },
+                {
+                    label: "Row",
+                    name: "row",
+                    widget: "list",
+                    fields: [
+                        {
+                            label: "Content",
+                            name: "rowContents",
+                            widget: "list",
+                            field: {
+                                label: "Row Content",
+                                name: "rowContent",
+                                widget: "markdown"
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    // Additionally, it's recommended that you use non-greedy capturing groups (e.g.
+  // `(.*?)` vs `(.*)`), especially if matching against newline characters.
+  pattern: /^<table>$\s*?<summary>(.*?)<\/summary>\n\n(.*?)\n^<\/table>$/ms,
+    fromBlock: function(match) {
+        console.lot("the fromBlock functionality", match)
+        return {};
+    },
+    toBlock: function(obj) {
+        console.log("the toBlock functionality", obj)
+        return `GORB`;
+    },
+    toPreview: function(obj) {
+        console.log("the toPreview functionality", obj)
+        return 'DOUBLE GORB';
+    },
+});
+
+
+
 
