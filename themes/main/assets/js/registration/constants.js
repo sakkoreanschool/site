999dc5e650068ebb7827837b66f9701cd830c5f9
kim2man <dunam.kim@gmail.com>
2024-07-30 21:27:41 -0700

    Change dbYear tab name to actual one (Prod)

--- themes/main/assets/js/registration/constants.js
+++ themes/main/assets/js/registration/constants.js
@@ -1,5 +1,5 @@
 export default {
-    dbYear: 'test_year',
+    dbYear: '24-25 1학기',
     previousDbYear: '23-24 1학기', // for re-register
     classes: ['유치가 (금|토)', '유치나', '기초1 (금)', '기초1 (토)', '기초2 (금|토)', '기초3', '초급 (금|토)', '중급', '고급'],
     username: "bgekitljcfbdtfrkufiuvfditbehbhtv",
