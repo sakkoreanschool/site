7674d61d73d3a64ea691661662fdcad423a6d4a2
Dunam Kim <dunam.kim@gmail.com>
2024-12-02 17:38:35 +0900

    Update registration DB config for 24-25 2nd Semester

--- themes/main/assets/js/registration/constants.js
+++ themes/main/assets/js/registration/constants.js
@@ -1,6 +1,6 @@
 export default {
-    dbYear: '24-25 1학기',
-    previousDbYear: '23-24 1학기', // for re-register
+    dbYear: '24-25 2학기',
+    previousDbYear: '24-25 1학기 prevDB', // for re-register
     classes: ['유치가 (금|토)', '유치나', '기초1 (금)', '기초1 (토)', '기초2 (금|토)', '기초3', '초급 (금|토)', '중급', '고급'],
     username: "bgekitljcfbdtfrkufiuvfditbehbhtv",
     auth: "fcenkjlbjujueijfrulgeghnhfhbfrre",
